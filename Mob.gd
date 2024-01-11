3e6fa93d6a88b3370bdfc4ce6e25a4cb3c2440f2
Anthony <anthony.vergne@outlook.com>
2024-01-11 16:26:50 -0500

    Improve collisions, camera and code readability

--- Mob.gd
+++ Mob.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+
 signal squashed
 
 @export var min_speed = 10.0
@@ -8,18 +9,26 @@ signal squashed
 func _physics_process(delta):
 	move_and_slide()
 
-func initialize(start_postion, player_position):
-	global_position = start_postion
-	look_at(player_position)
-	rotate_y(randf_range(-PI/4.0, PI/4.0))
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
+
+# This function will be called from the Main scene.
+func initialize(start_position, player_position):
+	# We position the mob by placing it at start_position
+	# and rotate it towards player_position, so it looks at the player.
+	look_at_from_position(start_position, player_position)
+	# Rotate this mob randomly within range of -45 and +45 degrees,
+	# so that it doesn't move directly towards the player.
+	rotate_y(randf_range(-PI / 4, PI / 4))
 	
-	var random_speed = randf_range(min_speed, max_speed)
+	# We generate a random speed (integer)
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
 	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 	
 func squash():
 	squashed.emit()
-	queue_free()
-
-func _on_visible_on_screen_notifier_3d_screen_exited():
 	queue_free()
--- player.gd
+++ player.gd
@@ -1,28 +1,26 @@
 extends CharacterBody3D
 
 
-@export var speed = 10.0
-@export var jump_velocity = 8.0
-@export var bounce_impulse = 4.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity : int = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export var speed = 14.0
+@export var jump_velocity = 18.0
+@export var bounce_impulse = 8.0
+@export var fall_acceleration = 75.0
 
 
 func _physics_process(delta):
+	get_velocity_from_input(delta)
+	
+	# Native func applying movement based on velocity
+	move_and_slide()
+	
+	handle_collisions()
+	
+# Get the input direction and store it in the native velocity variable
+func get_velocity_from_input(delta):
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	if direction:
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
@@ -30,15 +28,22 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-
-	move_and_slide()
 	
-	# Handling collisions
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= fall_acceleration * delta
+	else:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = jump_velocity
+
+func handle_collisions():
+	# Get all collisions since last processing
 	for index in range(get_slide_collision_count()):
 		var collision = get_slide_collision(index)
 		var collider = collision.get_collider()
 		
-		# Detect is the player collided with a mob
+		# Detect if the player collided with a mob
 		if collider.is_in_group("mobs"):
 			# Detect if the collision happened from above
 			if Vector3.UP.dot(collision.get_normal()) > 0.1 :
